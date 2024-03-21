e959f17f9ddefc99e7638feb53192cd34448b653
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-20 19:19:03 -0700

    Filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,9 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        if obj is None:
+            return
+        if obj in self.__objects:
+            self.__objects.remove(obj)
