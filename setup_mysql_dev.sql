1f18368f6c161dac0e393e457f77754c7e1b51bb
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-20 17:25:03 -0700

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -15,7 +15,7 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- hbnb_dev has SELECT privilege on the database performance_schema(only)
-GRANT SELECT ON preformance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
 -- Flush privileges for changes
 FLUSH PRIVILEGES;
