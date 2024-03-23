02ae0fbed9312a33362987f118f0eb152514103e
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-23 14:19:29 -0700

    DBStorage - States and Cities

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,18 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import declarative_base
+from sqlalchemy import Base, Column, Integer, String
 
+
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), nullable=False, primary_key)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
