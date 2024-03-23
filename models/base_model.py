bf1e619140806d3960087c8fd118685e6c4bade9
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-23 15:13:38 -0700

    updates

--- models/base_model.py
+++ models/base_model.py
@@ -13,14 +13,27 @@ class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), nullable=False, primary_key=True, unique=True)
     created_at = Column(DateTime, default=(datetime.utcnow()), nullable=False)
-    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=(datetime.utcnow()), nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        """Instatntiates a new base model based on arguments passed
+
+           Args:
+                args: unused
+                kwargs: arguments for the constructor of BaseModel
+
+           Attributes:
+                id - A unique identifier
+                created_at - creation date
+                update_at - update date
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if (key == "created_at" or key == "updated_at"):
+                    value = datetime.strptime(value, %Y-%m-%dT%H:%M:%S:.%f)
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
