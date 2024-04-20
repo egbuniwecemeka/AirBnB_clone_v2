b5e0806417ee9a525b940b0f8786c6c05eae5fe1
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-04-20 05:29:20 -0700

    CCCCCCCCCCCCCCCCsks

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,9 +5,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def greetings():
-    return <p>Hello HBNB</>
+    return "<p>Hello HBNB</p>"
 
-if __name__ = "__main__":
-    app.run(debug=True, port=0.0.0.0)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000)
