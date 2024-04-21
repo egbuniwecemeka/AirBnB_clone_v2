de272ecd9a0e2f21c1d4b69a2ee5e4e3b122add7
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-04-21 13:53:39 -0700

    Improve engines

--- models/state.py
+++ models/state.py
@@ -22,7 +22,7 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="states", cascade="all delete")
+        cities = relationship("City", backref="states", cascade="all, delete")
     else:
         @property
         def cities(self):
