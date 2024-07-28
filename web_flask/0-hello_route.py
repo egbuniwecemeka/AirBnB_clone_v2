b5e5e87592ada8eac912afaf82e737f86b37f517
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-07-27 22:35:50 -0700

    hello hbnb

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 
-# Imported Flask class
+""" A Flask application that returns Hello HBNB! """
+
 from flask import Flask
 
-# created an instance of Flask class
+# Creates a flask instance
 app = Flask(__name__)
 
 
 # route tell Flask the URL that triggers our function.
 @app.route('/', strict_slashes=False)
 def hello():
-    # Returns message to user browser. Default's HTML
+    """ Returns message to user browser. Default's HTML """
     return "Hello HBNB!"
 
 
 # run if script is executed directlly
 if __name__ == "__main__":
-    # Listen on 0.0.0.0 and port 5000
+    """ Listen on 0.0.0.0 and port 5000 """
     app.run(host='0.0.0.0', port=5000)
