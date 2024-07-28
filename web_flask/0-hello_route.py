bd1ba21d6a65167e42ee357d1fee8af110d0c01a
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-07-27 21:49:08 -0700

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,30 +1,18 @@
-<<<<<<< HEAD
-from flask import Flask
-''' Script thst runs an app with flask'''
-
-app = Flask(__name__)
-
-@app.route("/", strict_slashes=False)
-def hello_world():
-    ''' Function called through the \ route'''
-    return "Hello, HBNB!"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-=======
 #!/usr/bin/python3
-""" Python script that starts a Flask web application """
 
+# Imported Flask class
 from flask import Flask
 
+# created an instance of Flask class
 app = Flask(__name__)
 
+# route tell Flask the URL that triggers our function.
+@app.route('/', strict_slashes=False)
 
-@app.route("/", strict_slashes=False)
-def greetings():
+# Returns message to user browser. Default's HTML
+def hello():
     return "Hello HBNB!"
 
-
+# run if script is executed directlly
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
->>>>>>> cf1fc677ef1197ef3f1d764ad5bfd068db10d086
+    app.run(port=5000)
