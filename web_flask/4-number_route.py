353aec548691a4e7689cc34f1b31b6fbccee7d6f
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-07-28 21:58:46 -0700

    number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,49 +1,47 @@
 #!/usr/bin/python3
-"""
-    A python script that starts a Flask application:
-    Requirements:
-    - You must use the option strict_slashes=False in your route definition
-    Routes:
-    -  /: display “Hello HBNB!”
-    - /hbnb: display “HBNB”
-    - /c/<text>: display “C ”, followed by the value of the text variable
-    (replace underscore _ symbols with a space )
-    - /python/(<text>): display “Python ”, followed by the value of the text
-    variable (replace underscore _ symbols with a space).
-    Note: default value of text="is cool"
-    - /number/<n>: display “n is a number” only if n is an integer
-"""
+
+""" A flask application that returns HTML response """
+
 from flask import Flask
+
+# Creates the flass instance
 app = Flask(__name__)
 
 
+# Routes to a root URL
 @app.route('/', strict_slashes=False)
 def home():
-    return "Hello HBNB!"
+    string = "Hello HBNB!"
+    return f"{string}"
 
 
+# Routes to a URL of a method.(Meaniful URL / Better user experience)
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    string = "HBNB"
+    return f"{string}"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+# Routes to a URL of a method and uses variable as keyword argument
+@app.route('/c/<string:text>', strict_slashes=False)
 def c(text):
-    replaced_text = text.replace('_', ' ')
-    return f"C {replaced_text}"
+    return f"{text.replace('_', ' ')}"
 
 
+# Routes to a URL of a method and uses a variable as keyword argument
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
 def python(text):
-    replaced_text = text.replace('_', ' ')
-    return f"Python {replaced_text}"
+    return f"Python {text.replace('_', ' ')}"
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
+# Route to /number method of a URL
+@app.route('/number/<integer:n>', strict_slashes=False)
 def number(n):
-    return f"{n} is a number"
+    if n:
+        return f"{n} is a number"
 
 
+# Runs if script is executed as main program
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
