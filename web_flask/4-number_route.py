ab4cb655386add84ce70786a540f1c6abab0f13b
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-07-28 22:10:36 -0700

    number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 # Routes to a root URL
 @app.route('/', strict_slashes=False)
 def home():
+    """ Returns HTML content """
     string = "Hello HBNB!"
     return f"{string}"
 
@@ -18,6 +19,7 @@ def home():
 # Routes to a URL of a method.(Meaniful URL / Better user experience)
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ Returns HTML content """
     string = "HBNB"
     return f"{string}"
 
@@ -25,6 +27,7 @@ def hbnb():
 # Routes to a URL of a method and uses variable as keyword argument
 @app.route('/c/<string:text>', strict_slashes=False)
 def c(text):
+    """ Returns HTML but replace _ with indent """
     return f"{text.replace('_', ' ')}"
 
 
@@ -32,12 +35,14 @@ def c(text):
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<string:text>', strict_slashes=False)
 def python(text):
+    """ Returns HTML content but replace _ with indent"""
     return f"Python {text.replace('_', ' ')}"
 
 
 # Route to /number method of a URL
 @app.route('/number/<integer:n>', strict_slashes=False)
 def number(n):
+    """ Returns HTML content if n is an integer"""
     if n:
         return f"{n} is a number"
 
