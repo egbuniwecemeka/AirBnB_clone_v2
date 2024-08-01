e543b6bd781bb3a30d5325e6b10e43ced7ae53a8
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-07-31 21:30:53 -0700

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,55 +1,58 @@
 #!/usr/bin/python3
-"""
-    A python script that starts a Flask applicattion:
-    Requirements:
-    - You must use the option strict_slashes=False in your route definition
-    Routes:
-    - /: display “Hello HBNB!”
-    - /hbnb: display “HBNB”
-    - /c/<text>: display “C ”, followed by the value of the text variable
-    (replace underscore _ symbols with a space)
-    - /python/(<text>): display “Python ”, followed by the value of the
-    text variable (replace underscore _ symbols with a space)
-    The default value of text is “is cool”
-    - /number/<n>: display “n is a number” only if n is an integer
-    - /number_template/<n>: display a HTML page only if n is an integer:
-    H1 tag: “Number: n” inside the tag BODY
-"""
-from flask import Flask, render_template
+
+""" A flask application that returns HTML response """
+
+from flask import Flask
+
+# Creates the flass instance
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def home():
-    return "Hello HBNB!"
+    """ Returns HTML response from URL """
+    string = "Hello HBNB!"
+    return f"{string}"
 
 
+# Routes to a URL of a method.(Meaniful URL / Better user experience)
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    """ Returns HTML response from URL """
+    string = "HBNB"
+    return f"{string}"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+# Routes to a URL of a method and uses variable as keyword argument
+@app.route('/c/<string:text>', strict_slashes=False)
 def c(text):
-    replaced_text = text.replace('_', ' ')
-    return f"C {replaced_text}"
+    """ Returns HTML response from URL """
+    return f"{text.replace('_', ' ')}"
 
 
+# Routes to a URL of a method and uses a variable as keyword argument
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
 def python(text):
-    replaced_text = text.replace('_', ' ')
-    return f"Python {replaced_text}"
+    """ Returns HTML response from URL """
+    return f"Python {text.replace('_', ' ')}"
 
 
+# Routes to a URL that triggers the function
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    return f"{n} is a number"
+    """ Returns HTML response from URL """
+    if n:
+        return f'{n} is a number'
 
 
+# Routes to a template that triggers the function
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template('5-number.html', n=n)
+    """ Returns HTML response from URL """
+    return render_template('5-number.html', name=n)
 
 
+# Runs if script is executed as main program
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
